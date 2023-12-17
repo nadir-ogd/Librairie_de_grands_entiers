4b59d1db05adac22c832fcbde46b6e5628a8b297
nadir-ogd <jn_ouagued@esi.dz>
2023-12-17 23:20:05 +0100

    cmp done

--- main.c
+++ main.c
@@ -2,4 +2,27 @@
 
 int main(){
 
-}
+    bigint a,b;
+
+    a.size = 5;
+    b.size = 5;
+
+    a.value = (unsigned int*)malloc(a.size * sizeof(unsigned int));
+    b.value = (unsigned int*)malloc(b.size * sizeof(unsigned int));
+    
+    a.value[0] = 111111111;
+    a.value[1] = 222222222;
+    a.value[2] = 333333333;
+    a.value[3] = 444444444;
+    b.value[0] = 111111111;
+    b.value[1] = 222222222;
+    b.value[2] = 333333333;
+    b.value[3] = 444444444;
+    a.value[4] = 66666666;
+    b.value[4] = 66666666;
+
+    printf("cmp(a ; b) = %d\n",cmp(a,b));
+
+    free(a.value);
+    free(b.value);
+    }
--- src/lib.c
+++ src/lib.c
@@ -1,15 +1,65 @@
 #include "./lib.h"
 
 int cmp(bigint a, bigint b){
-    
+    if (a.size > b.size){//a > b 
+        for(unsigned int i = 0; i < b.size; i++)
+            if (a.value[i] != b.value[i])
+                return i;
+            return b.size;
+    }
+    else if (a.size < b.size) {//b > a
+        for(unsigned int i = 0; i < a.size; i++)
+            if (a.value[i] != b.value[i])
+                return -i;
+            return -(a.size);
+    }
+    else{//a > b ou b < a
+        for(unsigned int i = 0; i < a.size; i++){
+            if (a.value[i] > b.value[i])
+                return i;
+            if (a.value[i] < b.value[i])
+                return -i;
+        }
+    }
+
+    return 0;    
 }
 
 bigint add(bigint a, bigint b){
+    bigint c;
+    if (a.size > b.size){
+        c.size = a.size;
+        c.value = (unsigned int*)malloc(c.size * sizeof(unsigned int));
+        for (unsigned int i = 0; i < b.size; i++)
+            c.value[i] = a.value[i] + b.value[i];
 
+        for (unsigned int i = b.size; i < c.size; i++)
+            c.value[i] = a.value[i];
+    }
+    else{
+        c.size = a.size;
+        c.value = (unsigned int*)malloc(c.size * sizeof(unsigned int));
+        for (unsigned int i = 0; i < a.size; i++)
+            c.value[i] = a.value[i] + b.value[i];
+
+        for (unsigned int i = a.size; i < c.size; i++)
+            c.value[i] = b.value[i];
+    }
+    return c;
 }
 
 bigint sub(bigint a, bigint b){
+    bigint c;   
+    if (cmp(a,b) >= 0){
+        c.size = a.size;
+        c.value = (unsigned int*)malloc(c.size * sizeof(unsigned int));
+        for (unsigned int i = 0; i < b.size; i++)
+            c.value[i] = a.value[i] - b.value[i];
 
+        for (unsigned int i = b.size; i < c.size; i++)
+            c.value[i] = a.value[i];
+    }
+    return c;
 }
 
 bigint product(bigint a, bigint b){
