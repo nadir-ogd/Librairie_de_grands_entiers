4a69c5e89a88b74e0c5e4c0eab05c2c147bf7ff0
nadir-ogd <jn_ouagued@esi.dz>
2023-12-17 17:46:07 +0100

    setting project architecture

---
+++ lib.c
@@ -0,0 +1,21 @@
+#include "./lib.h"
+
+int cmp(bigint a, bigint b);
+
+bigint add(bigint a, bigint b);
+
+bigint sub(bigint a, bigint b);
+
+bigint product(bigint a, bigint b);
+
+void intdiv(bigint a, bigint b, bigint *quotient, bigint *modulo);
+
+bigint pow2n(unsigned int n); 
+
+void printbigint(bigint n); 
+
+char *biginttostr(bigint n); 
+
+bigint *strtobigint(char *s);
+
+char *biginttosubstr(bigint n, int first, int last);
---
+++ lib.h
@@ -0,0 +1,27 @@
+#ifndef LIB_H
+#define LIB_H
+
+/* Type bigint */
+typedef struct bigint {
+unsigned int size; /* taille */
+unsigned int *value; /* valeur */
+} bigint;
+
+/* Operators */
+int cmp(bigint a, bigint b); /* Comparaison */
+bigint add(bigint a, bigint b); /* Addition */
+bigint sub(bigint a, bigint b); /* Soustraction (a >= b)*/
+bigint product(bigint a, bigint b); /* Produit */
+/* Division */
+void intdiv(bigint a, bigint b, bigint *quotient, bigint *modulo);
+bigint pow2n(unsigned int n); /* 2 puissance n */
+/* Input / Output */
+void printbigint(bigint n); /* Affichage sortie standard */
+char *biginttostr(bigint n); /* Conversion bigint vers chaine */
+bigint *strtobigint(char *s); /* Conversion chaine vers bigint */
+/* Conversion bigint vers chaine */
+/* Decimales entre first et last */
+char *biginttosubstr(bigint n, int first, int last);
+
+
+#endif
