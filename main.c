a86e4aa19e00d979f5da72e88fc422f2b8ab6a5e
nadir-ogd <jn_ouagued@esi.dz>
2023-12-21 13:51:48 +0100

    some modifications in cmp function

--- main.c
+++ main.c
@@ -4,47 +4,31 @@ int main(){
 
     bigint a,b;
 
-    a.size = 3;
     b.size = 3;
+    a.size = 2;
 
     a.value = (unsigned int*)malloc(a.size * sizeof(unsigned int));
     b.value = (unsigned int*)malloc(b.size * sizeof(unsigned int));
     
-    a.value[2] = 999999999;
-    a.value[1] = 333333333;
-    a.value[0] = 4444444;
-    b.value[2] = 999999999;
-    b.value[1] = 333333333;
-    b.value[0] = 1111111;
-    // b.value[4] = 9999999;
-    // b.value[5] = 9999999; 
-    // a.value[3] = 444444444;
-    // b.value[0] = 111111111;
-    // b.value[1] = 222222222;
-    // b.value[2] = 333333333;
-    // b.value[3] = 444444444;
-    // a.value[4] = 66666666;
-    // b.value[4] = 66666666;
+    a.value[0] = 1;
+    a.value[1] = 2;
+    b.value[0] = 1;
+    b.value[1] = 1;
+    b.value[2] = 12;
+
     
-    bigint c = add(a, b);
+    printf("cmp(a, b) = %d\n",cmp(a, b));
 
-    printf("< ADD >\n");
-    printf("Taille de c = %d\n",c.size);
+    // bigint c = product(a, b);
+    // printf("\n< PRODUCT >\n");
+    // printf("a * b = ");
+    // for(int i = c.size-1; i >= 0; i--)
+    //     printf("%d",c.value[i]);
 
-    for(int i = c.size-1; i >= 0; i--)
-        printf("%d",c.value[i]);
-
-    bigint d = sub(a, b);
-
-    printf("\n< SUB >\n");
-    printf("Taille de d = %d\n",d.size);
-
-    for(int i = d.size-1; i >= 0; i--)
-        printf("%d",d.value[i]);
-
+    // printf("\n");
 
     free(a.value);
     free(b.value);
-    free(c.value);
-    free(d.value);
+    // free(c.value);
+
     }
--- src/lib.c
+++ src/lib.c
@@ -5,7 +5,6 @@ unsigned int countDigits(unsigned int x) {
     if (x == 0) {
         return 1;
     }
-
     unsigned int count = 0;
 
     while (x > 0) {
@@ -30,85 +29,136 @@ unsigned int power(unsigned int base, unsigned int exponent) {
     return result;
 }
 
-int cmp(bigint a, bigint b){
-    if (a.size > b.size){//a > b 
-        for(unsigned int i = 0; i < b.size; i++)
-            if (a.value[i] != b.value[i])
-                return i;
-        return b.size;//cas ou b est contenu dans a
-    }
-    else if (a.size < b.size) {//b > a
-        for(unsigned int i = 0; i < a.size; i++)
-            if (a.value[i] != b.value[i])
-                return -i;
-        return -(a.size);//cas ou a est contenu dans b
-    }
-    else{//a > b ou b < a
-        for(unsigned int i = 0; i < a.size; i++){
-            if (a.value[i] > b.value[i])
-                return i;
-            if (a.value[i] < b.value[i])
-                return -i;
+
+void copyAndFormat(bigint original, unsigned int *result) {
+    unsigned int ind = 0;
+
+    // Copier et formater les chiffres
+    for (unsigned int i = 0; i < original.size; i++) {
+        unsigned int val = original.value[i];
+
+        // Copier chaque chiffre individuel dans le tableau résultant
+        while (val != 0) {
+            result[ind++] = val % 10;
+            val /= 10;
         }
     }
-
-    return 0;    
 }
 
-// bigint add(bigint a, bigint b){
-//     bigint c;
-//     unsigned int carry = 0, nb_digit_a = 0, nb_digit_b = 0, max_nb_digits = 0;
-//     if (a.size >= b.size){
-//         c.size = a.size;
-//         c.value = (unsigned int*)malloc(c.size * sizeof(unsigned int));
+int cmp(bigint a, bigint b) {
+    unsigned int a_bit_size = 0, b_bit_size = 0;
 
-//         for (unsigned int i = 0; i < b.size; i++){
-//             nb_digit_a = countDigits(a.value[i]);
-//             nb_digit_b = countDigits(b.value[i]);
-//             if (nb_digit_a > nb_digit_b)
-//                 max_nb_digits = nb_digit_a;
-//             else
-//                 max_nb_digits = nb_digit_b;
-            
-//             printf("Pour a[%d] + b[%d] : %d + %d\n",i,i,a.value[i],b.value[i]);
-//             printf("nb_digit_a  = %d\n",nb_digit_a);
-//             printf("nb_digit_b  = %d\n",nb_digit_b);
-//             printf("max_nb_digits  = %d\n",max_nb_digits);
-//             c.value[i] = a.value[i] + b.value[i] + carry;
+    for (unsigned int i = 0; i < a.size; i++)
+        a_bit_size += countDigits(a.value[i]);
+    for (unsigned int i = 0; i < b.size; i++)
+        b_bit_size += countDigits(b.value[i]);
 
-//             printf("c.value avant = %d\n",c.value[i]);
+    unsigned int *a_copy = (unsigned int *)malloc(a_bit_size * sizeof(unsigned int));
+    unsigned int *b_copy = (unsigned int *)malloc(b_bit_size * sizeof(unsigned int));
 
-//             if (countDigits(c.value[i]) > max_nb_digits && i != c.size-1){
-//                 carry = 1;
-//                 c.value[i] = c.value[i] % power(10, max_nb_digits);;
-//             }
-//             else
-//                 carry = 0;
-            
-//             printf("c.value après = %d\n",c.value[i]);
+    // unsigned int ind = 0;
+    // for (unsigned int i = 0; i < a.size; i++) {
+    //     unsigned int val = a.value[i];
+    //     while (val != 0) {
+    //         a_copy[ind++] = val % 10;
+    //         val /= 10;
+    //     }
+    // }
 
-//         }
+    // ind = 0;
+    // for (unsigned int i = 0; i < b.size; i++) {
+    //     unsigned int val = b.value[i];
+    //     while (val != 0) {
+    //         b_copy[ind++] = val % 10;
+    //         val /= 10;
+    //     }
+    // }
+    copyAndFormat(a, a_copy);
+    copyAndFormat(b, b_copy);
+    
+    printf("\na = ");
+    for(unsigned int i = 0; i < a_bit_size; i++)
+        printf("%d",a_copy[i]);
+    
+    printf("\nb = ");
+    for(unsigned int i = 0; i < b_bit_size; i++)
+        printf("%d",b_copy[i]);
 
-//         c.value[b.size] = a.value[b.size] + carry;
-//         for (unsigned int i = b.size+1; i < c.size; i++)
-//           c.value[i] = a.value[i];
+    printf("\n");
 
-//         printf("c = ");
-//         for(int i = c.size-1; i >= 0; i--){
-//              printf("%d",c.value[i]);
-//         }
-//     }
-//     else{
-//         c.size = a.size;
-//         c.value = (unsigned int*)malloc(c.size * sizeof(unsigned int));
-//         for (unsigned int i = 0; i < a.size; i++)
-//             c.value[i] = a.value[i] + b.value[i];
+    unsigned int pos = 0;
+    unsigned int a_nb = 0, b_nb = 0;
 
-//         for (unsigned int i = a.size; i < c.size; i++)
-//             c.value[i] = b.value[i];
-//     }
-//     return c;
-// }
+
+    if (a_bit_size == b_bit_size) {
+        unsigned int base = 1;
+
+        for (unsigned int i = 0; i < a_bit_size; ++i) {
+            a_nb += a_copy[i] * base;
+            b_nb += b_copy[i]* base;
+            base *= 10;
+        }
+
+        printf("a_nb = %d\n",a_nb);
+        printf("b_nb = %d\n",b_nb);
+
+        if(a_nb == b_nb)
+            return 0;
+        else{
+            for (unsigned int i = 0; i < a_bit_size; i++)
+                if (a_copy[i] != b_copy[i]) {
+                    pos = i;
+                    break;
+                }
+            unsigned int j = 0;
+            if (a_nb > b_nb){
+                for (unsigned int i = 0; i < a.size; i++){
+                    for(unsigned int j = i; j < countDigits(a.value[i]); j++){
+                        if (j == pos)
+                            return (i + 1);
+                    }
+                }
+            }
+            else{
+                for (unsigned int i = 0; i < a.size; i++){
+                    for(unsigned int j = i; j < countDigits(a.value[i]); j++){
+                        if (j == pos)
+                            return -(i + 1);
+                    }
+                }
+            }
+        }
+    } else if (a_bit_size > b_bit_size) {
+        for (unsigned int i = 0; i < b_bit_size; i++)
+            if (a_copy[i] != b_copy[i]) {
+                pos = i;
+                break;
+            }
+        unsigned int j = 0;
+        for (unsigned int i = 0; i < a.size; i++) {
+            for (j = i; j < a_bit_size; j++) {
+                if (j == pos)
+                    return (i + 1);
+            }
+        }
+    } else {
+        for (unsigned int i = 0; i < b_bit_size; i++)
+            if (a_copy[i] != b_copy[i]) {
+                pos = i;
+                break;
+            }
+        unsigned int j = 0;
+        for (unsigned int i = 0; i < a.size; i++) {
+            for (j = i; j < a_bit_size; j++) {
+                if (j == pos)
+                    return -(i + 1);
+            }
+        }
+    }
+    free(a_copy);
+    free(b_copy);
+    return 0;
+}
 
 bigint add(bigint a, bigint b) {
     bigint c;
@@ -184,9 +234,8 @@ bigint sub(bigint a, bigint b){
         if (diff < 0) {
             borrow = 1;
             diff += power(10, max_nb_digits);
-        } else {
+        } else
             borrow = 0;
-        }
 
         c.value[i] = diff;
     }
@@ -199,9 +248,149 @@ bigint sub(bigint a, bigint b){
     return c;
 }
 
-bigint product(bigint a, bigint b){
+// bigint product(bigint a, bigint b){
+//     bigint c;
+//     c.size = a.size;
+//     c.value = (unsigned int *)malloc(c.size * sizeof(unsigned int));
+    
+//     bigint one;
+//     one.size = 1;
+//     one.value = (unsigned int *)malloc(one.size * sizeof(unsigned int));
+//     one.value[0] = 1;
+    
+//     bigint zero;
+//     zero.size = 1;
+//     zero.value = (unsigned int *)malloc(zero.size * sizeof(unsigned int));
+//     one.value[0] = 0;
 
+//     bigint tmp;
+//     tmp.size = b.size;
+//     tmp.value = (unsigned int *)malloc(tmp.size * sizeof(unsigned int));
+
+//     for (unsigned int i = 0; i < tmp.size; i++)
+//         tmp.value[i] = b.value[i];
+
+//     printf("Avant c : ");
+//     for(int i = c.size-1; i >= 0; i--)
+//         printf("%d",c.value[i]);
+
+//     printf("\nAvant tmp : ");
+//     for(int i = tmp.size-1; i >= 0; i--)
+//         printf("%d",tmp.value[i]);
+
+//     c = add(zero, zero);
+//     if (cmp(a, b) > 0){
+//         while(cmp(tmp, zero) != 0){
+//             c = add(c, a);
+//             tmp = sub(tmp, one);
+
+//             printf("\nDans boucle c = ");
+//             for(int i = c.size-1; i >= 0; i--)
+//                 printf("%d",c.value[i]);
+
+//             printf("\nDans boucle tmp = ");
+//             for(int i = tmp.size-1; i >= 0; i--)
+//                 printf("%d",tmp.value[i]);
+
+//             printf("\n cmp(tmp, zero) = %d\n", cmp(tmp, zero));
+//         }
+//     }
+
+//     printf("\nAprès c = ");
+//     for(int i = c.size-1; i >= 0; i--)
+//         printf("%d",c.value[i]);
+    
+//     printf("\nAprès tmp = ");
+//     for(int i = tmp.size-1; i >= 0; i--)
+//         printf("%d",tmp.value[i]);
+//     return c;
+// }
+
+bigint product(bigint a, bigint b) {
+    bigint one;
+    one.size = 1;
+    one.value = (unsigned int *)calloc(one.size, sizeof(unsigned int));
+    one.value[0] = 1;
+
+    bigint zero;
+    zero.size = b.size;
+    zero.value = (unsigned int *)calloc(zero.size, sizeof(unsigned int));
+    zero.value[0] = 0;
+
+    bigint tmp;
+    tmp.size = b.size;
+    tmp.value = (unsigned int *)calloc(tmp.size, sizeof(unsigned int));
+         
+    bigint c;
+    c.size = a.size + b.size;
+    c = add(zero, zero);
+
+    printf("\nc avant = ");
+    for(int i = c.size-1; i >= 0; i--)
+        printf("%d",c.value[i]);
+
+    printf("\nb avant = ");
+    for(int i = b.size-1; i >= 0; i--)
+        printf("%d",b.value[i]);
+
+    printf("\ntmp avant = ");
+    for(int i = tmp.size-1; i >= 0; i--)
+        printf("%d",tmp.value[i]);
+
+    printf("\ncmp(tmp, zero) = %d\n",cmp(tmp, zero));
+    while (cmp(b, tmp) != 0) {
+        c = add(c, a);
+        tmp = add(tmp, one);
+
+        printf("\nc après = ");
+        for(int i = c.size-1; i >= 0; i--)
+            printf("%d",c.value[i]);
+        
+        printf("\ntmp après = ");
+        for(int i = tmp.size-1; i >= 0; i--)
+            printf("%d",tmp.value[i]);
+
+        printf("\none après = ");
+        for(int i = one.size-1; i >= 0; i--)
+            printf("%d",one.value[i]);
+            
+    }
+
+    free(one.value);
+    free(zero.value);
+    free(tmp.value);
+
+    return c;
 }
+
+
+
+
+// bigint product(bigint a, bigint b) {
+//     bigint c;
+//     c.size = a.size + b.size;
+//     c.value = (unsigned int *)calloc(c.size, sizeof(unsigned int));
+
+//     for (unsigned int i = 0; i < a.size; i++) {
+//         unsigned int carry = 0;
+//         for (unsigned int j = 0; j < b.size; j++) {
+//             unsigned long long p = (unsigned long long)a.value[i] * b.value[j] + c.value[i + j] + carry;
+//             c.value[i + j] = p % power(10, 9);
+//             carry = p / power(10, 9);
+//             printf("p = %u | c.value[%d + %d] = %d | carry = %d\n",p,i,j,c.value[i+j],carry);
+//         }
+//         c.value[i + b.size] += carry;
+//     }
+    
+//     // Retirer les zéros non significatifs à gauche
+//     while (c.size > 1 && c.value[c.size - 1] == 0) {
+//         c.size--;
+//         c.value = realloc(c.value, c.size * sizeof(unsigned int));
+//     }
+
+//     return c;
+// }
+
 
 void intdiv(bigint a, bigint b, bigint *quotient, bigint *modulo){
 
